318f1e2351547e0b3667e6a65d8278679f91a2fe
tojawawel <bgawel93@gmail.com>
2018-11-23 12:39:01 +0100

    add welcome page layout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'bc12f3c80e22dee4ec6bacebb362b0db8f190b622b62ca3b62e87957857f944e962ea9b982ee68d47ede441d88e5041974fc2e5bce5465beb2cb42d44fb0ba11'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'notreply@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
